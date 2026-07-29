383aee6933a8e43ef9dee8b159b7f434146ca723
volkov <gdarksandr@gmail.com>
2026-07-29 20:38:38 +0400

    update migrate

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"main/pkg"
 	"math/rand"
 
@@ -8,6 +9,7 @@ import (
 )
 
 func main() {
+	log.Println("Старт миграции")
 	pkg.Init()
 
 	db := pkg.OpenDB()
@@ -21,31 +23,51 @@ func main() {
 
 	db.AutoMigrate(tables...)
 
-	image := "https://upload.wikimedia.org/wikipedia/commons/1/11/Test-Logo.svg"
+	users := []*pkg.User{}
+	news := []*pkg.News{}
 
+	userAvatar := []string{
+		"/static/img/features-fashion.webp",
+		"/static/img/features-life-style.webp",
+		"/static/img/features-sports-1.webp",
+		"/static/img/lifestyle-1.webp",
+		"/static/img/lifestyle-2.webp",
+	}
+
+	newsImage := []string{
+		"/static/img/news-1.webp",
+		"/static/img/news-2.webp",
+		"/static/img/news-3.webp",
+		"/static/img/news-4.webp",
+		"/static/img/news-5.webp",
+		"/static/img/news-6.webp",
+		"/static/img/news-7.webp",
+		"/static/img/news-8.webp",
+	}
+
+	log.Println("Заполняем пользователей")
 	for range 100 {
-		user := pkg.User{
+		users = append(users, &pkg.User{
 			Name:      randomdata.FullName(randomdata.RandomGender),
 			About:     randomdata.Paragraph(),
-			AvatarUrl: image,
-		}
-		db.Create(&user)
-
-		news_count := rand.Intn(5)
-		news := []*pkg.News{}
-		for range news_count {
-			news = append(news, &pkg.News{
-				Title:      randomdata.SillyName(),
-				Content:    randomdata.Paragraph(),
-				ImageUrl:   image,
-				ReadTime:   rand.Intn(10) + 1,
-				Views:      rand.Intn(1000),
-				ShareCount: rand.Intn(100),
-				UserID:     user.ID,
-			})
-		}
-		if len(news) > 0 {
-			db.Create(&news)
-		}
+			AvatarUrl: userAvatar[rand.Intn(len(userAvatar)-1)],
+		})
 	}
+	db.Create(&users)
+
+	log.Println("Заполняем новости")
+	for range 1000 {
+		news = append(news, &pkg.News{
+			Title:      randomdata.SillyName(),
+			Content:    randomdata.Paragraph(),
+			ImageUrl:   newsImage[rand.Intn(len(newsImage)-1)],
+			ReadTime:   rand.Intn(10) + 1,
+			Views:      rand.Intn(1000),
+			ShareCount: rand.Intn(100),
+			UserID:     users[rand.Intn(len(users)-1)].ID,
+		})
+	}
+	db.Create(&news)
+
+	log.Println("Конец миграции")
 }
